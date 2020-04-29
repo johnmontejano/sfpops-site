b3b3da81d91969882d96075ba9010eb866f852ed
John Montejano <johnmontejano@Johns-MacBook-Pro.local>
2020-04-28 19:24:36 -0700

    header component built

---
+++ src/Title.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Title.css';
+function Title() {
+  return (
+    <div className="Title">
+      <header>
+        <h1>SFPOPS</h1>
+        <div className="Title-Subtitle">
+          <h3>San Francisco Privately Owned Public Open Spaces</h3>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export { Title };
