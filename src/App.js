fb4cea2670fa0902b34f982144b83807a6f2cfb6
John Montejano <johnmontejano@Johns-MacBook-Pro.local>
2020-05-01 09:57:21 -0700

    [add] content component built

--- src/App.js
+++ src/App.js
@@ -2,25 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Title } from './Title.js';
+import { POPOSList } from './POPOSList.js';
 function App() {
   return (
     <div className="App">
       <Title />
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <POPOSList />
     </div>
   );
 }
